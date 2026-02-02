35489a153f99313d1c706f9a18a2db40205419f1
ruam <roamsaleh@gmail.com>
2026-02-02 12:16:14 +0300

    added assets

--- AlNaham/views/SplashScreenView.swift
+++ AlNaham/views/SplashScreenView.swift
@@ -1,7 +1,24 @@
 //
-//  SplashScreen.swift
+//  SplashScreenView.swift
 //  AlNaham
 //
 //  Created by ruam on 14/08/1447 AH.
 //
 
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = true
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body : some View {
+        
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
