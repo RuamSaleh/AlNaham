b7f86a5220ccdc264627814023cc8b9110f5e184
Ghadeer Fallatah <ghadeer1781@gmail.com>
2026-02-03 11:41:59 +0300

    Home UI

---
+++ AlNaham/views/StartJourney.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  AlNaham
+//
+//  Created by Ghadeer Fallatah on 15/08/1447 AH.
+//
+
+import SwiftUI
+
+struct StartJourney: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+#Preview {
+    StartJourney()
+}
