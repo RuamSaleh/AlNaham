ddee4567552e3f456015adaecce859b1d73c3cab
ruam <roamsaleh@gmail.com>
2026-02-03 12:09:48 +0300

    splashScreen

--- AlNaham/views/SplashScreenView.swift
+++ AlNaham/views/SplashScreenView.swift
@@ -8,17 +8,61 @@
 import SwiftUI
 
 struct SplashScreenView: View {
-    @State private var isActive = true
-    @State private var size = 0.8
-    @State private var opacity = 0.5
+    @State private var animateSea = false
+    @State private var cloudsIn = false
+    @State private var shipIn = false
     
-    var body : some View {
-        
+    var body: some View {
+        ZStack {
+            ZStack {
+                Image("cloudL")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 426)
+                    .offset(x: cloudsIn ? -12 : 400)
+                
+                Image("cloudR")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 426)
+                    .offset(x: cloudsIn ? 12 : -400)
+            }
+            .padding(.top, 59)
+            .onAppear {
+                withAnimation(.easeOut(duration: 1)) {
+                    cloudsIn = true
+                }
+            }
+       
+            ZStack {
+                Image("sea")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 852, height: 393)
+                    .offset(y: animateSea ? 226 : 400)             }
+            
+            .onAppear {
+                withAnimation(.easeOut(duration: 1)) {
+                    animateSea = true
+                }
+            }
+            
+            Image("ship")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 426)
+                .offset(x: shipIn ? -120 : -900, y: 150)
+                .onAppear {
+                    withAnimation(.easeOut(duration: 1)) {
+                        shipIn = true
+                    }
+                }
+        }
     }
-}
-
-struct SplashScreenView_Previews: PreviewProvider {
-    static var previews: some View {
-        SplashScreenView()
+    
+    struct SplashScreenView_Previews: PreviewProvider {
+        static var previews: some View {
+            SplashScreenView()
+        }
     }
 }
